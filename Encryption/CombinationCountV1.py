de16bef633b542cf15d69127d7296b7b09b181b6
Sean P. Myrick V19.0.3.1 <65933340+seanpm2001@users.noreply.github.com>
2020-07-02 18:48:33 -0700

    4 main files
    4 main files for this project, the homepage [2x] in HTML format, and the combination count [2x] in Python format.

---
+++ Encryption/CombinationCountV1.py
@@ -0,0 +1,13 @@
+# encryption password combination
+utf8 = int(1112064) # Number of UTF-8 character points
+combinationlen = int(1000000) # 1 MB
+print (utf8 * combinationLen)
+# print (utf8 ** combinationlen)
+noMore = input("Press [ENTER] key to quit")
+# End of script
+''' Still haven't found the correct combination yet '''
+# Done in Python, as it can handle very large integer calculations (but not float calculations past the machines processor (32, 64, or 128 bit))
+''' File version: 1 (October 8th 2019)
+First version: 1 (October 8th 2019)
+Latest version: 1 (October 8th 2019)
+'''
